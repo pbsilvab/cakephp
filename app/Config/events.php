411fb0cd5537bea1466321c261406dc2fe1b2ff1
pedro <pbsilvab@gmail.com>
2019-04-09 10:43:31 -0300

    LogListener event

--- app/Config/events.php
+++ app/Config/events.php
@@ -1,7 +1,8 @@
 <?php 
     // Load event listeners
-App::uses('PostListener', 'Lib/Event');
+//App::uses('PostListener', 'Lib/Event');
+App::uses('LogListener', 'Lib/Event');
 App::uses('CakeEventManager', 'Event');
 
 // Attach listeners.
-CakeEventManager::instance()->attach(new PostListener());
+CakeEventManager::instance()->attach(new LogListener());
