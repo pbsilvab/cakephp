d7d3526c5fe981e8456d18706ef2d813030674b2
pedro <pbsilvab@gmail.com>
2019-04-09 14:44:20 -0300

    Shell configuration and task exec

---
+++ app/Console/Command/HelloShell.php
@@ -0,0 +1,10 @@
+<?php
+    class HelloShell extends AppShell {
+    
+        public $uses = array('User');
+
+        public function main() {
+            $user = $this->User->find('all');
+            $this->out(print_r($user, true));
+        }
+    }
